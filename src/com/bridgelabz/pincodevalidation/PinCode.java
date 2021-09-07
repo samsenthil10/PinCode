3be3291e340470391a4a891803d9b9106d587496
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-08 01:23:01 +0530

    Added: To validate pincode 400088 using regex

---
+++ src/com/bridgelabz/pincodevalidation/PinCode.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.pincodevalidation;
+
+import java.util.regex.Pattern;
+
+public class PinCode {
+	
+	public static void validatePinCode(String pincode) {
+		
+		boolean result = Pattern.matches("[0-9]{6}", pincode);
+		if(result == true) {
+			System.out.println("Valid Pincode");
+		}
+		else {
+			System.out.println("Invalid Pincode");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		validatePinCode("400088");
+	}
+
+}
