11ea1336a12fa0fd34cea81ea6db84ff4b1c2604
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-08 01:27:00 +0530

    Modified: To ensure regular expression rejects pincode A400088

--- src/com/bridgelabz/pincodevalidation/PinCode.java
+++ src/com/bridgelabz/pincodevalidation/PinCode.java
@@ -3,9 +3,9 @@ package com.bridgelabz.pincodevalidation;
 import java.util.regex.Pattern;
 
 public class PinCode {
-	
+
 	public static void validatePinCode(String pincode) {
-		
+
 		boolean result = Pattern.matches("[0-9]{6}", pincode);
 		if(result == true) {
 			System.out.println("Valid Pincode");
@@ -16,8 +16,8 @@ public class PinCode {
 	}
 
 	public static void main(String[] args) {
-		
-		validatePinCode("400088");
-	}
 
+		validatePinCode("400088");
+		validatePinCode("A400088");
+	}
 }
